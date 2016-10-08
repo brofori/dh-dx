eadd22e6e5b62b545e3740890a815c2d584623d1
Tobias Tilly <tobias@tilly-hamburg.de>
2016-10-08 21:45:07 +0200

    Wrickwrackwruck

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django_mongoengine import Document, EmbeddedDocument, fields
 
-class User(Document):
+class Users(Document):
     email = fields.StringField(required=True, unique=True)
     first_name = fields.StringField(max_length=50)
     last_name = fields.StringField(max_length=50)
     birthday = fields.DateTimeField()
+    gender = fields.StringField()
--- users/views.py
+++ users/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
-from users.models import User
-
-def user_profile(request,user_id):
-    user = User.objects.first(user_id)
+from users.models import Users
 
 
+def user_serializer(user):
+    return {'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'birthday': user.birthday,
+            }
+
+
+def user_profile(request, user_id):
+    user = Users.objects.get(id=user_id).first()
     response = HttpResponse(user)
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+def user_list(request):
+    users = list(Users.objects.all())
+    users = [user_serializer(user) for user in users]
+    response = HttpResponse(users)
     response["Access-Control-Allow-Origin"] = "*"
     return response
