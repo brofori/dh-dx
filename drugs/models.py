93925fe623e6e352b80e17ca86baf013fc0e9189
Tobias Tilly <tobias@tilly-hamburg.de>
2016-10-08 16:14:20 +0200

    added activities

--- drugs/models.py
+++ drugs/models.py
@@ -2,10 +2,11 @@ from mongoengine import Document
 from mongoengine.base.fields import BaseField
 from mongoengine.fields import ListField, StringField, ReferenceField
 
+TYPES = ['vaccination,pill,juice,injection']
 
 class Drugs(Document):
     name = StringField(required=True, max_length=200)
-    type = BaseField(choices=['vaccination,pill,juice,injection'])
+    type = BaseField(choices=TYPES)
     sideEffects = ListField()
     schedule = ListField()
     interactions = ListField(ReferenceField('self'))
