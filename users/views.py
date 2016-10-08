5c0dcc1b85a70f9d429b1558d5c6d0a1745fbf80
Tobias Tilly <tobias@tilly-hamburg.de>
2016-10-08 21:22:23 +0200

    COOOOOOOORS

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-
+from users.models import User
 
 def user_profile(request,user_id):
-    response = HttpResponse('bitch '+user_id)
+    user = User.objects.first(user_id)
+
+
+    response = HttpResponse(user)
+    response["Access-Control-Allow-Origin"] = "*"
     return response
